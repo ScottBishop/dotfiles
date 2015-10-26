cdc8ea4a4df3b774f39660494184582f4b0ab5a7
ScottBishop <scottbishop70@gmail.com>
2015-10-26 15:27:36 -0700

    Added git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,40 +6,59 @@ then
   alias git=$hub_path
 fi
 
-# The rest of my fun git aliases
-alias gl='git pull --prune'
+# Logging
+alias gl='git log --first-parent'
+alias glg='git log --graph'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+
+# Pushing
 alias gp='git push origin HEAD'
+function gpsu() { git push --set-upstream origin "$(git_branch)" }
+
+# Pulling
+alias gpl='git pull'
+alias gsr='git svn rebase'
+
+# Status
 alias gd='git diff'
+alias gs='git status'
+
+# Committing
 alias gc='git commit'
 alias gca='git commit -a'
+alias gaap='git commit -a --ammend --no-edit'
 alias gcam='git commit -am'
+alias gcm='git commit -m'
+alias ga='git add --all'
+
+# Branching
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias gw='./gradlew --offline'
 
-alias git=hub
-alias gitoneline="git log --graph --decorate --pretty=format:\"%C(auto)%h%d %Cblue%an%Creset: %C(auto)%s\""
-alias gitonelineall="git log --graph --decorate --all --pretty=format:\"%C(auto)%h%d %Cblue%an%Creset: %C(auto)%s\""
-alias gitpretty="git log --graph --decorate --name-status"
-alias gitprettyall="git log --graph --decorate --name-status --all"
-alias gitreset="git reset HEAD\^" # convenience function to go back one commit
-alias gitpush="git push origin HEAD"
-alias gitwip="git commit -a -m 'WIP DO NOT COMMIT'"
+# Clean up
+alias grp='git remote prune origin'
+alias gcl='git-cleanup'
 
-function gitmergecommit() { git log $1..HEAD --ancestry-path --merges }
-function gitmerged() { git branch --merged $@ | sed -e '/^*/d' }
-function gitcleanup() { git branch -d $(gitmerged); git remote prune origin }
-function gitshowsvn() { git show `git svn find-rev r$1` }
-function gitsvnrebase() {
-	if [[ $1 != "-l" ]]; then
-		git svn fetch;
-	fi
-	git for-each-ref --shell --format='git co %(refname:short); git svn rebase -l;' refs/heads | \
-		while read entry
-		do
-			eval "$entry"
-		done
+function git_branch() {
+  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+}
+
+function git-merged() { git branch --merged $@ | sed -e '/^*/d' }
+
+function git-cleanup() { 
+	echo "=== Cleaning Remote Branch Caches ==="
+	git remote prune origin
+
+	echo "=== Cleaning Local Branches ========="
+	except_branches=('"\*"' 'master' 'develop' 'rc')
+	command="git branch --merged"
+	for branch in $except_branches; do
+		command="$command | grep -v $branch"
+	done
+	command="$command | xargs -n 1 git branch -d"
+	eval $command
+
+	echo "=== Remaining Branches =============="
+	git branch
 }
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -27,6 +27,7 @@ export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 export PATH=/usr/local/bin:$PATH
 
 eval "$(rbenv init -)"
+eval "$(thefuck --alias fuck)"
 
 export MARKPATH=$HOME/.marks
 function jump { 
